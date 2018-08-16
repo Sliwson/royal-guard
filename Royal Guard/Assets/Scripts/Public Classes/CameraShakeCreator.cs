e69708921ede1486d2bdedeae05be31dfa2f5f32
Sliwson <mateusz_sliw@interia.pl>
2018-08-16 11:57:35 +0200

    Add own instance creator to EzCameraShake asset

---
+++ Royal Guard/Assets/Scripts/Public Classes/CameraShakeCreator.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using EZCameraShake;
+
+[System.Serializable]
+class CameraShakeCreator
+{
+    public float magnitude;
+    public float roughness;
+    public float fadeInTime;
+    public float fadeOutTime;
+    public Vector3 positionInfluence;
+    public Vector3 rotationInfluence;
+    public bool deleteOnInactive;
+
+    public CameraShakeInstance GetInstance()
+    {
+        CameraShakeInstance cameraShakeInstance = new CameraShakeInstance(magnitude, roughness, fadeInTime, fadeOutTime);
+        cameraShakeInstance.PositionInfluence = positionInfluence;
+        cameraShakeInstance.RotationInfluence = rotationInfluence;
+        cameraShakeInstance.DeleteOnInactive = deleteOnInactive;
+        return cameraShakeInstance;
+    }
+}
