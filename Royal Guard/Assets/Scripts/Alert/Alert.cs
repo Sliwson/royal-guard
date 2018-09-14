378dfb4ee85bc075f4d90d2975f133021b675b95
Sliwson <mateusz_sliw@interia.pl>
2018-09-14 14:01:03 +0200

    Add script that calculate projectile position outside the screen

--- Royal Guard/Assets/Scripts/Alert/Alert.cs
+++ Royal Guard/Assets/Scripts/Alert/Alert.cs
@@ -10,11 +10,14 @@ public class Alert : MonoBehaviour {
 
     private bool fadedOut = false;
 
-    private Coordinates screenCorners;
     private AlertAnimationController alertAnimationController;
 
     public void SpawnAlert(Transform transform)
     {
+        ProjectilePositions position = CalculateProjectilePosition(transform);
+
+
+
         alertAnimationController = new AlertAnimationController(GetComponent<Animator>());
         alertAnimationController.TriggerAnimation(AlertAnimations.FadeIn);
     }
@@ -27,5 +30,41 @@ public class Alert : MonoBehaviour {
     public void DestroyAlert()
     {
         Destroy(gameObject);
+    }
+
+    private ProjectilePositions CalculateProjectilePosition(Transform projectileTransform)
+    {
+        Vector3 leftTop = Coordinates.GetCameraCorner(Corner.LeftTop);
+        Vector3 rightBottom = Coordinates.GetCameraCorner(Corner.RightBottom);
+
+        Vector3 projectilePosition = projectileTransform.position;
+
+        if (projectilePosition.x < leftTop.x)
+        {
+            if (projectilePosition.y > leftTop.y)
+                return ProjectilePositions.LeftUp;
+            else if (projectilePosition.y < rightBottom.y)
+                return ProjectilePositions.LeftDown;
+            else
+                return ProjectilePositions.Left;
+        }
+        else if (projectilePosition.x > rightBottom.x)
+        {
+            if (projectilePosition.y > leftTop.y)
+                return ProjectilePositions.RightUp;
+            else if (projectilePosition.y < rightBottom.y)
+                return ProjectilePositions.RightDown;
+            else
+                return ProjectilePositions.Right;
+        }
+        else
+        {
+            if (projectilePosition.y > leftTop.y)
+                return ProjectilePositions.Up;
+            else if (projectilePosition.y < rightBottom.y)
+                return ProjectilePositions.Down;
+            else
+                return ProjectilePositions.OnCamera;
+        }
     }
 }
---
+++ Royal Guard/Assets/Scripts/Enums/ProjectilePositions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public enum ProjectilePositions
+{
+    Left,
+    Up,
+    Right,
+    Down,
+    LeftUp,
+    RightUp,
+    RightDown,
+    LeftDown,
+    OnCamera
+}
